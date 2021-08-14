c073ddbf24759e2e2dff17115f12a48164872676
tripdragon <nickitdesign@gmail.com>
2021-08-14 02:50:28 -0400

    fixn

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ exports.deployment = async ({ start } = {}) => {
     const manifest = Manifest.get('/', process.env);
     const server = await Glue.compose(manifest, { relativeTo: __dirname });
 
+    // server.subscription('/fishes');
+    server.subscription('/tacos');
+
     if (start) {
         await Exiting.createManager(server).start();
         server.log(['start'], `Server started at ${server.info.uri}`);
